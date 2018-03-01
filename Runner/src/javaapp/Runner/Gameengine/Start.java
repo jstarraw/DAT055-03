3fc57a4ec61073be3ce243fc9f29d95a207bea33
nurabdchalemrs <nurab@student.chalmers.se>
2018-03-01 13:00:25 +0100

    Communication

--- Runner/src/javaapp/Runner/Gameengine/Start.java
+++ Runner/src/javaapp/Runner/Gameengine/Start.java
@@ -1,10 +1,14 @@
 package javaapp.Runner.Gameengine;
 
+import java.io.IOException;
+
 public class Start {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		Gameengine game = new Gameengine("Runner", 822, 668);
 		game.start();
+	
+	
 	}
 
 }
--- Runner/src/javaapp/Runner/States/MenuState.java
+++ Runner/src/javaapp/Runner/States/MenuState.java
@@ -1,24 +1,27 @@
 package javaapp.Runner.States;
 
 import java.awt.Graphics;
+import java.io.IOException;
 
 import javaapp.Runner.Gameengine.Assets;
 import javaapp.Runner.Gameengine.Gameengine;
+import javaapp.Runner.Gameengine.Highscore;
 
 public class MenuState extends States {
 
 	private States gameState;
 	private States highscore;
+	private int localtimer=10000;
 
 	public MenuState(Gameengine game) {
 		super(game);
 		gameState = new Gamestate(game);
-		highscore = new Highscore(game);
+		//highscore = new Highscore(game);
 	}
 
 	@Override
 	public void tick() {
-
+		localtimer++;
 		// button.getButton();
 		if (game.getKeyManager().isPlay()) {
 			States.setState(gameState);
@@ -29,9 +32,15 @@ public class MenuState extends States {
 			System.exit(0);
 
 		}
-
-		if (game.getKeyManager().isHighscore()) {
-			States.setState(highscore);
+		
+		if (game.getKeyManager().isHighscore() && localtimer >10000) {
+			try {
+				Highscore.getHighscore();
+				localtimer=0;
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 
 		}
 
