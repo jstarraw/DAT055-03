97f1192997a85e9247ed6a8803a04bd168190e92
nurabdchalemrs <nurab@student.chalmers.se>
2018-03-01 14:08:16 +0100

    allmostdone_room3

--- Runner/src/javaapp/Runner/Room3/Doors_Room3.java
+++ Runner/src/javaapp/Runner/Room3/Doors_Room3.java
@@ -1,73 +1,62 @@
 package javaapp.Runner.Room3;
-
-import java.awt.Graphics;
-
-import javaapp.Runner.Gameengine.Assets;
+		
+import java.awt.Graphics;		
+import javaapp.Runner.Gameengine.Assets;	
 
 public class Doors_Room3 {
-
-	private float x, y;
-
-	public Doors_Room3(float x, float y) {
-		this.x = x;
-		this.y = y;
-
-	}
-
-	public void tickLeftdoor() {
-
+private float x ;
+			
+public Doors_Room3( float x ){
+		this.x=x;
+				}
+	/**
+	 * 		
+	 */
+	public void tickLeftdoor(){
+				
 		x++;
-		// if(x > 430){
-		// setX(430);
-		//
-	}
-
-	// }
-	public void tickRightdoor() {
-
+	if(x > 430)x=430;
+			}
+	/**
+	 * 
+	 */
+	public void tickRightdoor(){
 		x--;
-		// if(x < 285){
-		// setX(285);
-		// }
-
-	}
-
-	// public void closeLeftdoor(){
-	//
-	//
-	// if(x > 430){
-	// x--;
-	//
-	// }
-	//
-	// }
-	//
-
-	public void renderDoorleft(Graphics g) {
-
-		g.drawImage(Assets.door1, (int) x, (int) y, null);
-
-	}
-
-	public void renderDoorRight(Graphics g) {
-
-		g.drawImage(Assets.door2, (int) x, (int) y, null);
-	}
-
-	public float getX() {
-		return x;
-	}
-
-	public void setX(float x) {
-		this.x = x;
-	}
-
-	public float getY() {
-		return y;
-	}
-
-	public void setY(float y) {
-		this.y = y;
-	}
-
+		if(x < 285)setX(285);
 }
+/**
+ * 
+ * @param g
+ */
+			
+public void  renderDoorleft(Graphics g){
+				
+	g.drawImage(Assets.door1, (int)x, (int) 45, null);
+}
+
+/**
+ * 
+ * @param g
+ */
+public void  renderDoorRight(Graphics g){
+		g.drawImage(Assets.door2, (int) x, (int) 45, null);
+	}
+
+/**
+ * 
+ * @return
+ */
+public float getX() {
+	
+	return x;
+}
+	/**
+	 * 	
+	 * @param x
+	 */
+public void setX(float x) {
+	
+				this.x = x;
+			}
+	
+		}
