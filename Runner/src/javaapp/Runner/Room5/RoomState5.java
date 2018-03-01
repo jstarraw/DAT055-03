6b38b94a050e2ebf0e48f4ee7c03a17aca1af3c9
RickardGyllensten <36076914+RickardGyllensten@users.noreply.github.com>
2018-03-01 16:42:44 +0100

    fix

--- Runner/src/javaapp/Runner/Room5/RoomState5.java
+++ Runner/src/javaapp/Runner/Room5/RoomState5.java
@@ -18,9 +18,7 @@ import javaapp.Runner.States.Victory;
 public class RoomState5 extends States {
 
 	private static boolean flag1, flag2, flag3;
-<<<<<<< HEAD
 	private States victory;
-=======
 	
 	/**
 	 * 
@@ -29,7 +27,6 @@ public class RoomState5 extends States {
 	 * 
 	 */
 
->>>>>>> 25cc7ac9cf11eddb25b5d357bcbac1f24dafef49
 	public RoomState5(Gameengine game) {
 		super(game);
 		victory = new Victory(game);
